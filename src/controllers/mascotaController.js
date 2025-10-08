e456fdd0e69206ddcc7a3ae3f2d8af0e377b1ea3
Adriana Camarillo Mora <adrianacamarillo@conceptosti.com>
2025-10-08 08:45:33 -0600

    Base del proyecto, CRUDs de mascotas y productos

---
+++ src/controllers/mascotaController.js
@@ -0,0 +1,51 @@
+import Mascota from "../models/Mascota.js";
+
+export const listarMascotas = async (req, res) => {
+  try {
+    const filtros = req.query;
+    const mascotas = await Mascota.find(filtros);
+    res.json(mascotas);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+//Prueba local de que corre bien el proyecto descomentar para prueba
+/*export const listarMascotas = async (req, res) => {
+  try {
+    // Datos falsos de ejemplo
+    const mascotas = [
+      { id: 1, nombre: "Firulais", tipo: "perro" },
+      { id: 2, nombre: "Michi", tipo: "gato" }
+    ];
+    res.json(mascotas);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};*/
+
+export const crearMascota = async (req, res) => {
+  try {
+    const nueva = await Mascota.create(req.body);
+    res.json(nueva);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+export const actualizarMascota = async (req, res) => {
+  try {
+    const mascota = await Mascota.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(mascota);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+export const eliminarMascota = async (req, res) => {
+  try {
+    await Mascota.findByIdAndDelete(req.params.id);
+    res.json({ mensaje: "Mascota eliminada" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
