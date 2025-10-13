26795ad200027677526ad550096664624b01db87
Victor Leyva <biktopwinner@gmail.com>
2025-10-12 20:24:47 -0600

    Feature / Actualizar dependencias y agregar configuración de API y conexion a BD con mongoose

---
+++ src/config/api.config.js
@@ -0,0 +1,9 @@
+const API_CONFIG = {
+    SERVER_PORT: process.env.PORT,
+    MONGO_URI: process.env.MONGO_URI,
+    DB_NAME: process.env.NAME_DB,
+    DB_USER: process.env.USER_DB,
+    DB_PASSWORD: process.env.PASS_DB,
+}
+
+export default API_CONFIG;
