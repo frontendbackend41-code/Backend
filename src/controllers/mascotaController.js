279aeb866248200f3a92fb78a5758848b1e9ab41
Adriana Camarillo Mora <adrianacamarillo@conceptosti.com>
2025-10-14 12:00:53 -0600

    Feature/se modifican los campos de mascota y se editan controladores

--- src/controllers/mascotaController.js
+++ src/controllers/mascotaController.js
@@ -1,50 +1,45 @@
 import Mascota from "../models/Mascota.js";
-
+//listado de mascotas
 export const listarMascotas = async (req, res) => {
   try {
-    const filtros = req.query;
-    const mascotas = await Mascota.find(filtros);
-    res.json(mascotas);
+    const filtros = {};
+    const { especie, sexo, estadoAdopcion} = req.query;
+
+    if (especie) filtros.especie = especie;
+    if (sexo) filtros.sexo = sexo;
+    if (estadoAdopcion) filtros.estadoAdopcion = estadoAdopcion;
+
+    const mascota = await Mascota.find(filtros);
+    res.json(mascota);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 };
-//Prueba local de que corre bien el proyecto descomentar para prueba
-/*export const listarMascotas = async (req, res) => {
-  try {
-    // Datos falsos de ejemplo
-    const mascotas = [
-      { id: 1, nombre: "Firulais", tipo: "perro" },
-      { id: 2, nombre: "Michi", tipo: "gato" }
-    ];
-    res.json(mascotas);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};*/
-
+//crear mascota
 export const crearMascota = async (req, res) => {
   try {
     const nueva = await Mascota.create(req.body);
-    res.json(nueva);
+    res.status(200).json(nueva);
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
 };
-
+//actualizar mascota
 export const actualizarMascota = async (req, res) => {
   try {
     const mascota = await Mascota.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!mascota) return res.status(404).json({ error: "Mascota no encontrada" });
     res.json(mascota);
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
 };
-
+//eliminar mascota
 export const eliminarMascota = async (req, res) => {
   try {
-    await Mascota.findByIdAndDelete(req.params.id);
-    res.json({ mensaje: "Mascota eliminada" });
+    const mascota = await Mascota.findByIdAndDelete(req.params.id);
+    if (!mascota) return res.status(404).json({ error: "Mascota no encontrada" });
+    res.json({ mensaje: "Mascota eliminada correctamente" });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
