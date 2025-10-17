c5bbe789939cf282d48ba2d4db73e506192f9c20
Luis <jesuslara.luis99@gmail.com>
2025-10-16 21:04:26 -0600

    Adecuacion de vercel. v7

--- api/index.js
+++ api/index.js
@@ -1,5 +1,12 @@
 const serverless = require('serverless-http');
 const app = require('../src/app');
+const express = require("express");
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hola desde vercel")
+});
+
 
 module.exports.handler = serverless(app);
 
